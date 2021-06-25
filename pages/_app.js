bb6f29227b3452d14a8965f782a16bf346ed501b
ianlee92 <ethian9227@gmail.com>
2021-06-25 17:30:03 +0900

    리덕스 설치 셋팅

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,8 @@ import PropTypes from 'prop-types';
 import Head from 'next/head';
 import 'antd/dist/antd.css';
 
+import wrapper from '../store/configureStore';
+
 const App = ({ Component }) => {
     return (
         <>
@@ -20,4 +22,4 @@ App.propTypes = {
     Component: PropTypes.elementType.isRequired,
 }
 
-export default App;
+export default wrapper.withRedux(App);
---
+++ store/configureStore.js
@@ -0,0 +1,9 @@
+import { createWrapper } from 'next-redux-wrapper';
+
+const configureStore = () => {
+
+};
+
+const wrapper = createWrapper(configureStore, {debug: process.env.NODE_ENV === 'development',});
+
+export default wrapper;
